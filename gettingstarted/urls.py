f977feee915ed3eaf1d1e4bbb1c28a59a717ba6f
mkmicik <mkmicik@ualberta.ca>
2016-03-11 12:44:29 -0700

    Deploy

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,16 +1,19 @@
 from django.conf.urls import include, url
-
 from django.contrib import admin
-admin.autodiscover()
-
-import hello.views
-
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    # Examples:
+    #url(r'^$', 'author	s.views.user_login', name='login'),
+    #url(r'^$', include('stream.urls',namespace='stream')),
+    url(r'^$',include('stream.urls',namespace='stream')),
+    url(r'^login/','authors.views.user_login', name='login'),
+    url(r'^register/', 'authors.views.register', name='register'),
+    url(r'^logout/', 'authors.views.logout', name='logout'),
+    url(r'^friends/',include('friends.urls',namespace='friends')),
+    url(r'^settings/', 'settings.views.index', name='settings'),
+    url(r'^profile/', 'posts.views.show_posts', name='show_posts'),
+    url(r'^posts/', include('posts.urls', namespace='posts')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/',include('api.urls',namespace='api')),
+    url(r'^comments/', include('comments.urls', namespace='comments')),
 ]
