1a39a55e2c40770462571aa39bcc3799aebd3ccb
mkmicik <mkmicik@ualberta.ca>
2016-03-19 14:27:16 -0600

    Deserialization of posts works

--- stream/views.py
+++ stream/views.py
@@ -12,6 +12,7 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from posts.converter import PostConverter
 #from django.core import serializers
 
 import urllib2
@@ -33,7 +34,7 @@ def convert(post_dict):
 def getExternalPosts():
 
 	nodes = Node.objects.all()
-
+	postConv = PostConverter()
 	posts = []
 
 	for n in nodes:
@@ -50,9 +51,15 @@ def getExternalPosts():
 		serializer.is_valid()
 		
 		#deserialized = serializer.validated_data
-		print 'Deserialized Data: '
-		print serializer.data
+		#print 'Deserialized Data: '
+		#print serializer.data['posts'][0]
 		#print serializer.data['posts']
+		
+		for p in serializer.data['posts']:
+			p['server'] = 'Remote'
+			post = postConv.convert(p)
+			posts.append(post)
+
 
 		#deserialized = serializer.validated_data.items()[1]
 
@@ -82,13 +89,15 @@ def index(request):
 
 	for post in int_posts:
 		all_posts.append(post)
-	getExternalPosts()
+	
 	# Get external posts
-	ext_posts = Post.objects.filter(published__lte=timezone.now()).order_by('-published')#getExternalPosts()
+	ext_posts = getExternalPosts()# Post.objects.filter(published__lte=timezone.now()).order_by('-published')#getExternalPosts()
 	for post in ext_posts:
-		post.server = "Remote"
+		print post.comments
+	#	post.server = "Remote"
 		all_posts.append(post)
 
+	print all_posts[0].comments
 	list.sort(all_posts)
 	
 	# Combine the two lists in chronological order
