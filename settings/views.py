c8c2c77e0bbac09fb9bb04080ba3f8dd16a02d45
Kieran Boyle <kboyle@cs.ualberta.ca>
2016-03-26 17:31:54 -0600

    Editing profile git, username, password, all works now

--- settings/views.py
+++ settings/views.py
@@ -12,5 +12,29 @@ from django.contrib.auth.models import User
 
 @login_required
 def index(request):
+
 	author = Author.objects.get(user=request.user)
-	return render(request, 'settings/index.html', {'author':author, 'current_author':author})
+	user =  User.objects.get (id=request.user.id)
+
+	if request.method == 'POST':
+		#get all of the form elements
+		print "I cannot believe that this could work"
+		new_name =  request.POST['username']
+		new_git =  request.POST['git_hub']
+		new_passwd = request.POST['password']
+		#check if the username is not nothing
+		if len(new_name) > 0:
+			user.username = new_name
+		#check if they have a password empy
+		if len(new_passwd) > 0:
+			user.set_password(new_passwd)
+
+		#condolidate changes
+		author.github = new_git
+		author.save()
+		user.save()
+
+		#generate response
+		return HttpResponseRedirect('author/'+author.author_id)
+	else:
+		return render(request, 'settings/index.html', {'author':author, 'current_author':author})
